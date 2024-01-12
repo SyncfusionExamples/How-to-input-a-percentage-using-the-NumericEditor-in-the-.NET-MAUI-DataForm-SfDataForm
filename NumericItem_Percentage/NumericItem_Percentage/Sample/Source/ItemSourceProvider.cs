9a6e549e5b0ed42afcd86ab5dafd530a3653fa19
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2024-01-12 18:31:44 +0530

    Added sample.

---
+++ NumericItem_Percentage/NumericItem_Percentage/Sample/Source/ItemSourceProvider.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.Maui.DataForm;
+
+namespace NumericItem_Percentage
+{
+    class ItemSourceProvider : IDataFormSourceProvider
+    {
+        public object GetSource(string sourceName)
+        {
+            if (sourceName == "Tickets")
+            {
+                List<string> noOfTickets = new List<string>() { "1", "2", "3", "4", "5" };
+                return noOfTickets;
+            }
+            if (sourceName == "Venue")
+            {
+                List<string> venue = new List<string>() { "Los Angeles", "Seattle", "New York", "Denver" };
+                return venue;
+            }
+            if (sourceName == "PaymentMethod")
+            {
+                List<string> payment = new List<string>() { "Credit Card", "Debit Card", "PayPal" };
+                return payment;
+            }
+
+            return new List<string>();
+        }
+    }
+}
